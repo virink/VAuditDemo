be084555c6f0c60780f7f9cf7a90ce0143359464
Virink <virink@outlook.com>
2018-11-07 16:19:07 +0800

    add docker for Debug
    Signed-off-by: Virink <virink@outlook.com>

--- VAuditDemo_Debug/sys/config.php
+++ VAuditDemo_Debug/sys/config.php
@@ -2,23 +2,22 @@
 
 error_reporting(0);
 
-if (!file_exists($_SERVER["DOCUMENT_ROOT"].'/sys/install.lock')){
+if (!file_exists($_SERVER["DOCUMENT_ROOT"] . '/sys/install.lock')) {
 	header("Location: /install/install.php");
-    exit;
+	exit;
 }
 
-include_once($_SERVER["DOCUMENT_ROOT"].'/sys/lib.php');
+include_once '../sys/lib.php';
 
-$host="localhost"; 
-$username="root"; 
-$password="root"; 
-$database="vauditdemo"; 
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "vauditdemo";
 
-$conn = mysql_connect($host,$username,$password);
-mysql_query('set names utf8',$conn);
+$conn = mysql_connect($host, $username, $password);
+mysql_query('set names utf8', $conn);
 mysql_select_db($database, $conn) or die(mysql_error());
-if (!$conn)
-{
+if (!$conn) {
 	die('Could not connect: ' . mysql_error());
 	exit;
 }
